bb1cc38443668fc3273010be59345e2819b05f22
drthnn <a34358179@example.com>
2026-04-08 10:11:35 +0400

    Первый коммит: добавлен сайт для Telegram сообщества

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const PORT = process.env.PORT || 3000;
 // Middleware
 app.use(cors());
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static(__dirname));
 app.use('/uploads', express.static('uploads'));
 
 // Создаём папки
